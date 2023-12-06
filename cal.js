ca9a8713d0a1034bca5482beb925861561bab912
martinjacob21 <martinjacob2001mj@gmail.com>
2023-12-06 11:11:09 +0530

    first commit

---
+++ cal.js
@@ -0,0 +1,12 @@
+function displayData(e){
+    result.value+=e
+}
+function out(){
+    result.value=eval(result.value)
+}
+function Allclear(){
+    result.value=""
+}
+function backspace(){
+    result.value=result.value.slice(0,-1)
+}
